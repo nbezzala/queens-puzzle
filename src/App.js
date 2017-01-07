7b6033a779fdb31cb77a620390e162c6c5316469
Nitish Bezzala <nbezzala@yahoo.com>
2017-01-07 16:39:21 +1300

    a clickable 9 square board

--- src/App.js
+++ src/App.js
@@ -3,11 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class Square extends Component {
+
+    constructor() {
+        super();
+	    this.state = {
+	        value: null
+	    };
+    }
+	
     render() {
         return(
-            <button className="Square"/>
+            <button className="square" onClick={() => this.setState({value: 'X'}) }>{this.state.value}</button>
         );
     };
+}
+
+class Board extends Component {
+
+    renderSquare(i) {
+        return(<Square value={i} />);
+    }
+    render() {
+        return(
+            <div className="Board">
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
 }
 
 class App extends Component {
@@ -22,8 +58,8 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-<Square />
         </p>
+        <Board />
       </div>
     );
   }
